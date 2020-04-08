5a6c90368973d0d48a68829f82b8bae145e4b03b
ito-soichro <queen.fm1235@gmail.com>
2020-04-08 14:43:25 +0900

    doneメソッドで受取り

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,43 @@
 $(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      var html =
+                `<div class="main_chat__message__position1">
+                    <div class="main_chat__message__position1__user1">
+                      <div class="main_chat__message__position1__user1__name">
+                        ${message.user_name}
+                      </div>
+                      <div class="main_chat__message__position1__user1__date">
+                        ${message.created_at}
+                      </div>
+                   </div>
+                   <div class="main_chat__message__position1__text1">
+                      <p class="lower-message__content">
+                        ${message.content}
+                      </p>
+                  </div>
+                </div>`
+       return html;
+     } else {
+       var html = 
+                 `<div class="main_chat__message__position1">
+                    <div class="main_chat__message__position1__user1">
+                      <div class="main_chat__message__position1__user1__name">
+                        ${message.user_name}
+                      </div>
+                      <div class="main_chat__message__position1__user1__date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="main_chat__message__position1__text1">
+                      <p class="lower-message__content">
+                        ${message.content}
+                      </p>
+                    </div>
+                 </div>`              
+       return html;
+     };
+   }
   $('#new_message').on('submit', function(e){
     e.preventDefault() 
     var formData = new FormData(this);
